ecd3d4a58f52bf3c1a6f65d3a15cdc1e454ff859
PinoSchule <160234716+PinoSchule@users.noreply.github.com>
2024-03-07 15:52:53 +0100

    LevelWahl

---
+++ Pengu_neu/LevelWahl.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LevelWahl here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelWahl extends World
+{
+
+    /**
+     * Constructor for objects of class LevelWahl.
+     * 
+     */
+    public LevelWahl()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(750, 348, 1); 
+    }
+}
