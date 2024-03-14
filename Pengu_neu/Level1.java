bcca6a7461f603cdfceeae728a8f9523e37d32cd
PinoSchule <160234716+PinoSchule@users.noreply.github.com>
2024-03-14 16:28:40 +0100

    Boden

--- Pengu_neu/Level1.java
+++ Pengu_neu/Level1.java
@@ -17,8 +17,8 @@ public class Level1 extends World
     {    
         super(500, 500, 1); 
         addObject( new  Cliff(true), 85, 441);
-         addObject( new  RealityAnchor(), 100, 260);
-         addObject( new  RealityAnchor(), 200, 260);
+        addObject( new  RealityAnchor(), 100, 260);
+        addObject( new  RealityAnchor(), 200, 260);
         for(int i = 0; i < 50; i++){
             addObject(new Cloud(), i*10,1);
         }
@@ -27,5 +27,56 @@ public class Level1 extends World
         addObject( new  Pengu(), 66, 244);
         addObject(new LevelEnd(Level2.class),400, 250);
 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Cliff cliff = new Cliff();
+        addObject(cliff,78,483);
+        cliff.setLocation(71,477);
+        Cliff cliff2 = new Cliff();
+        addObject(cliff2,126,483);
+        cliff2.setLocation(120,478);
+        Cliff cliff3 = new Cliff();
+        addObject(cliff3,175,482);
+        cliff3.setLocation(168,478);
+        Cliff cliff4 = new Cliff();
+        addObject(cliff4,223,484);
+        cliff4.setLocation(216,478);
+        cliff4.setLocation(217,479);
+        cliff4.setLocation(216,479);
+        Cliff cliff5 = new Cliff();
+        addObject(cliff5,271,486);
+        cliff5.setLocation(265,478);
+        cliff5.setLocation(261,477);
+        cliff5.setLocation(263,479);
+        Cliff cliff6 = new Cliff();
+        addObject(cliff6,318,485);
+        cliff6.setLocation(311,480);
+        Cliff cliff7 = new Cliff();
+        addObject(cliff7,366,484);
+        cliff7.setLocation(358,479);
+        Cliff cliff8 = new Cliff();
+        addObject(cliff8,413,485);
+        cliff8.setLocation(406,477);
+        cliff8.setLocation(406,480);
+        Cliff cliff9 = new Cliff();
+        addObject(cliff9,461,487);
+        cliff9.setLocation(454,482);
+        Cliff cliff10 = new Cliff();
+        addObject(cliff10,494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(494,488);
+        cliff10.setLocation(499,482);
     }
 }
