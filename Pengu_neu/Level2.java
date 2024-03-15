21548c3e569915ee691b449ce59f67dab8ff3fae
PinoSchule <160234716+PinoSchule@users.noreply.github.com>
2024-03-15 13:06:41 +0100

    6

--- Pengu_neu/Level2.java
+++ Pengu_neu/Level2.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -102,3 +103,37 @@ public class Level2 extends World
         realityanchor.setLocation(602,22);
     }
 }
+=======
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Level1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Level2 extends World
+{
+
+    /**
+     * Constructor for objects of class Level1.
+     * 
+     */
+    public Level2()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(650, 650, 1); 
+        addObject( new  Pengu(), 0, 602);
+        int lim = 48;
+        for(int i =0; i<=650/lim;i++){
+    addObject( new  Cliff(), i*lim, 626);
+    addObject( new  Cliff(), i*lim, 500);
+    }
+    
+
+}
+    public Object getNextLevel(){
+        return new Level1();
+    }
+}
+>>>>>>> Stashed changes
