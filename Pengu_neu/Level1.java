0fea20df0355da54a518e2f1f0e801f915666798
PinoSchule <160234716+PinoSchule@users.noreply.github.com>
2024-03-15 11:58:36 +0000

    level1 addition

--- Pengu_neu/Level1.java
+++ Pengu_neu/Level1.java
@@ -36,47 +36,6 @@ public class Level1 extends World
      */
     private void prepare()
     {
-        Cliff cliff = new Cliff();
-        addObject(cliff,78,483);
-        cliff.setLocation(71,477);
-        Cliff cliff2 = new Cliff();
-        addObject(cliff2,126,483);
-        cliff2.setLocation(120,478);
-        Cliff cliff3 = new Cliff();
-        addObject(cliff3,175,482);
-        cliff3.setLocation(168,478);
-        Cliff cliff4 = new Cliff();
-        addObject(cliff4,223,484);
-        cliff4.setLocation(216,478);
-        cliff4.setLocation(217,479);
-        cliff4.setLocation(216,479);
-        Cliff cliff5 = new Cliff();
-        addObject(cliff5,271,486);
-        cliff5.setLocation(265,478);
-        cliff5.setLocation(261,477);
-        cliff5.setLocation(263,479);
-        Cliff cliff6 = new Cliff();
-        addObject(cliff6,318,485);
-        cliff6.setLocation(311,480);
-        Cliff cliff7 = new Cliff();
-        addObject(cliff7,366,484);
-        cliff7.setLocation(358,479);
-        Cliff cliff8 = new Cliff();
-        addObject(cliff8,413,485);
-        cliff8.setLocation(406,477);
-        cliff8.setLocation(406,480);
-        Cliff cliff9 = new Cliff();
-        addObject(cliff9,461,487);
-        cliff9.setLocation(454,482);
-        Cliff cliff10 = new Cliff();
-        addObject(cliff10,494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(494,488);
-        cliff10.setLocation(499,482);
+       
     }
 }
