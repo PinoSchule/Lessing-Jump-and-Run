71c898a7d262a2556f6d43a13acc563459af1cce
PinoSchule <160234716+PinoSchule@users.noreply.github.com>
2024-03-15 12:39:10 +0000

    diamond spike

--- Pengu_neu/Level1.java
+++ Pengu_neu/Level1.java
@@ -14,19 +14,14 @@ public class Level1 extends World
      * 
      */
     public Level1()
-    {    
-        super(650, 650, 1); 
-        addObject( new  Cliff(true), 85, 441);
-        addObject( new  RealityAnchor(), 100, 260);
-        addObject( new  RealityAnchor(), 200, 260);
-        for(int i = 0; i < 50; i++){
-            addObject(new Cloud(), i*10,1);
-        }
-        addObject( new  Cloud(), 369, 315);
-        addObject( new  Spike(0), 320, 315);
-        addObject( new  Pengu(), 66, 244);
-        addObject(new LevelEnd(Level2.class),400, 250);
+    {
 
+        super(650, 650, 1);
+        addObject( new  Pengu(), 66, 0);
+        int lim = 48;
+        for(int i =0; i<=650/lim;i++){
+            addObject( new  Cliff(), i*lim, 626);
+        }
         prepare();
     }
 
