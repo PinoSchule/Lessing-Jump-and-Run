e84e37fb0f30701c0c41b89ac53135390af0bd24
PinoSchule <160234716+PinoSchule@users.noreply.github.com>
2024-03-22 12:24:07 +0000

    Haus

---
+++ Pengu_neu/house.java
@@ -0,0 +1,31 @@
+
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * It's just a cliff.
+ */
+public class house extends Actor
+{
+
+    /**
+     * Default consrtuctor - nothign to do
+     */
+    public house()
+    {
+    }
+
+    /**
+     * Create a horizontally mirrored cliff face.
+     */
+
+
+    /**
+     * Act: nothing to do here (the cliff does not do anything)
+     */
+    public void act()
+    {
+        /* nothing to do*/
+    }
+}
